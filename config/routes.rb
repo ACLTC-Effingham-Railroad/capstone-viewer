9e54bc3cf03a4f691172552af2b728af30e3e296
nealdeters <nealdeters@gmail.com>
2016-02-18 20:39:44 -0600

    created capstone controller and model. deleted model migration file. added unirest and bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'capstones/index'
+
+  get 'capstones/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
